5754234a9e23cf0de18faf4b09e3fefd29eb2c19
RoyHawa <Hawaroy18@gmail.com>
2022-07-21 15:44:11 +0300

    css

--- src/Components/comments/Comments.js
+++ src/Components/comments/Comments.js
@@ -5,8 +5,6 @@ import { FaComments } from "react-icons/fa";
 import {
   loadCommentsForPostId,
   selectComments,
-  isLoadingComments,
-  errorLoadingComments,
 } from "../../features/subreddit/subredditSlice";
 
 const Comments = ({ postId, permalink, num_comments }) => {
@@ -14,8 +12,6 @@ const Comments = ({ postId, permalink, num_comments }) => {
   const dispatch = useDispatch();
   const comments = useSelector(selectComments);
   const loadComments = loadCommentsForPostId(permalink, postId);
-  const isLoading = useSelector(isLoadingComments);
-  const errorLoading = useSelector(errorLoadingComments);
 
   const handleClick = () => {
     let fetchedComments = comments.find(
@@ -27,38 +23,25 @@ const Comments = ({ postId, permalink, num_comments }) => {
     setShowComments(!showComments);
   };
 
-  if (isLoading && showComments) {
-    return (
-      <div className="lds-ellipsis">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-    );
-  }else if(errorLoading){
-    return <h1>Error</h1>
-  } else {
-    return (
-      <div>
-        <button onClick={handleClick}>
-          <FaComments />
-        </button>
-        {num_comments}
-        {showComments &&
-          comments
-            .find((commentObj) => commentObj.postId === postId)
-            .comments.map((comment, index) => {
-              return (
-                <div key={index} className="comment">
-                  <h5>{comment.author}</h5>
-                  <p>{comment.body}</p>
-                </div>
-              );
-            })}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <button onClick={handleClick}>
+        <FaComments />
+      </button>
+      {num_comments}
+      {showComments &&
+        comments
+          .find((commentObj) => commentObj.postId === postId)
+          .comments.map((comment, index) => {
+            return (
+              <div key={index} className="comment">
+                <h5>{comment.author}</h5>
+                <p>{comment.body}</p>
+              </div>
+            );
+          })}
+    </div>
+  );
 };
 
 export default Comments;
